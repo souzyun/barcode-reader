0a1e77f0ade931050925b2befdbe7c90549d8893
souzyun <souzyun@me.com>
2025-07-11 16:49:05 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
 function startQrScanner(pavilionName, scannerPage) {
-  const qrScanner = new Html5Qrcode("reader");
-
-  // カメラ設定
-  const config = {
-    fps: 15,
-    qrbox: { width: 280, height: 280 },
-    aspectRatio: 1.0,
-    useBarCodeDetectorIfSupported: true
-  };
-
-  // 旧インスタンス停止（念のため）
-  Html5Qrcode.getCameras().then(cameras => {
-    qrScanner.stop().catch(() => {});
-    
-    qrScanner.start(
-      { facingMode: "user" },
-      config,
-      (decodedText) => {
-        qrScanner.stop().then(() => {
-          window.location.href = `view.html?id=${decodedText}&pavilion=${pavilionName}&from=${scannerPage}`;
-        }).catch(() => {
-          window.location.href = `view.html?id=${decodedText}&pavilion=${pavilionName}&from=${scannerPage}`;
-        });
-      },
-      (errorMessage) => {
-        // 読み取り失敗（無視でOK）
-      }
-    );
-  });
+  const qrScanner = new Html5Qrcode("reader", { facingMode: "user" });
+  qrScanner.start(
+    { facingMode: "user" },
+    {
+      fps: 10,
+      qrbox: 250
+    },
+    (decodedText) => {
+      window.location.href = `view.html?id=${decodedText}&pavilion=${pavilionName}&from=${scannerPage}`;
+      qrScanner.stop();
+    },
+    (error) => {
+      // ignore
+    }
+  );
 }
