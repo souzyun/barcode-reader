b9dca3f7b953bea5cc53bbe8d5d75a661fdbdb93
souzyun <souzyun@me.com>
2025-07-14 16:06:36 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,7 +9,12 @@ function startQrScanner(pavilionName, scannerPage) {
     { facingMode: "user" },
     { fps: 10, qrbox: 250 },
     (decodedText) => {
-      entryAudio.play().catch((e) => console.log("音声再生エラー", e));
+      try {
+        entryAudio.currentTime = 0;
+        entryAudio.play().catch(err => console.log("音声再生エラー", err));
+      } catch (e) {
+        console.log("Audio制御エラー", e);
+      }
       window.location.href = `view.html?id=${decodedText}&pavilion=${pavilionName}&from=${scannerPage}`;
       qrScanner.stop();
     },
