fcdb8fb8172adf74cf58d39170c4d6bca6a97ee6
souzyun <souzyun@me.com>
2025-07-14 15:59:32 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
+let entryAudio;
+
 function startQrScanner(pavilionName, scannerPage) {
+  entryAudio = new Audio(`sounds/${scannerPage.replace(".html", ".mp3")}`);
+  entryAudio.preload = "auto";
+
   const qrScanner = new Html5Qrcode("reader", { facingMode: "user" });
   qrScanner.start(
     { facingMode: "user" },
-    {
-      fps: 10,
-      qrbox: 250
-    },
+    { fps: 10, qrbox: 250 },
     (decodedText) => {
+      entryAudio.play().catch((e) => console.log("音声再生エラー", e));
       window.location.href = `view.html?id=${decodedText}&pavilion=${pavilionName}&from=${scannerPage}`;
       qrScanner.stop();
     },
-    (error) => {
-      // ignore
-    }
+    (error) => {}
   );
 }
